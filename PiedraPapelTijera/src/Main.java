6722001d624bf6fbd67bd02ab730b7717beca40e
Victor <vmartina@iesch.org>
2022-10-18 20:18:18 +0200

    Buscar excepción que se da cuando metes un numero que no sea 1 2 3

--- PiedraPapelTijera/src/Main.java
+++ PiedraPapelTijera/src/Main.java
@@ -6,11 +6,12 @@ public class Main {
         try {
             System.out.println("Introduce el número correspondiente al objeto que quieres sacar. \n1 - Piedra \n2-Papel\n3-Tijera");
             Scanner sc = new Scanner(System.in);
-            int objeto = sc.nextInt();
+            int objeto = 0;
             final int PIEDRA = 1;
             final int PAPEL = 2;
             final int TIJERA = 3;
-            int ia = (int)(Math.random()* 3 + 1);
+            int ia;
+
             if (objeto == PIEDRA){
                 System.out.println("Has escogido piedra");
             }
@@ -20,13 +21,14 @@ public class Main {
             if (objeto == TIJERA){
                 System.out.println("Has escogido tijera");
             }
-            if (objeto >= 1 && objeto <= 3){
                 ia = (int)(Math.random()* 3 + 1);
                 if (ia == PIEDRA) System.out.println("El ordenador ha escogido piedra");
                 if (ia == PAPEL) System.out.println("El ordenador ha escogido papel");
                 if (ia == TIJERA) System.out.println("El ordenador ha escogido tijera");
-            }
-        }catch ()
+
+        }catch (RuntimeException e){
+            throw new RuntimeException();
+        }
 
 
     }
